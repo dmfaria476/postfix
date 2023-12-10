3e00647e6c41df94f978cff33cce87436e0d9db5
Daniel Faria <dmsfaria476@gmail.com>
2023-12-10 22:41:28 -0100

    v1

---
+++ volumes/config.local.php
@@ -0,0 +1,15 @@
+<?php
+                $CONF['database_type'] = 'mysqli';
+                $CONF['database_host'] = 'db';
+                $CONF['database_port'] = '3306';
+                $CONF['database_user'] = 'xpto';
+                $CONF['database_password'] = 'xpto';
+                $CONF['database_name'] = 'xpto';
+                $CONF['setup_password'] = '$2y$10$9OkcBE6KUkfT7O9MISc6PeEQbOW2Fm1WXQnagcaj/YhC/M.9xKh6C';
+                $CONF['smtp_server'] = 'localhost';
+                $CONF['smtp_port'] = '25';
+                $CONF['encrypt'] = 'md5crypt';
+                $CONF['configured'] = true;
+                $CONF['dkim'] = 'NO';
+                $CONF['dkim_all_admins'] = 'NO';
+                ?>
